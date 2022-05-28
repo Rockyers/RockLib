8469930b5a7fe7bd409f694ef34b497ab372ab07
Rockyers <ethan@yocom.org>
2022-05-27 20:17:08 -0700

    SubCommand Annotations

--- src/main/java/me/rockyers/rocklib/abstracts/SubCommand.java
+++ src/main/java/me/rockyers/rocklib/abstracts/SubCommand.java
@@ -1,6 +1,11 @@
 package me.rockyers.rocklib.abstracts;
 
 import org.bukkit.entity.Player;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.List;
 
 /**
@@ -11,29 +16,54 @@ import java.util.List;
  */
 public abstract class SubCommand {
     /**
+     * TODO: DOCUMENT
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public @interface SubCommandInfo {
+        String name();
+        String description();
+        String syntax();
+        String permission() default "";
+        String permessionmessage() default "";
+    }
+
+    SubCommandInfo info = this.getClass().getAnnotation(SubCommandInfo.class);
+
+    /**
      * Name of the subcommand, the actual input in chat
-      */
-    public abstract String getName();
+     */
+    public String getName() {
+        return info.name();
+    }
 
     /**
      * A short description of what the SubCommand does
-      */
-    public abstract String getDescription();
+     */
+    public String getDescription() {
+        return info.description();
+    }
 
     /**
      * The full command, including the parent command (I plan on removing this soon)
      */
-    public abstract String getSyntax();
+    public String getSyntax() {
+        return info.syntax();
+    }
 
     /**
      * The permission required to run the SubCommand, set to "" or null for no permission
      */
-    public abstract String getPermission();
+    public String getPermission() {
+        return info.permission();
+    }
 
     /**
      * The message sent to the player if they try to run the SubCommand without the required permission
      */
-    public abstract String getNoPermMessage();
+    public String getNoPermMessage() {
+        return info.permessionmessage();
+    }
 
     /**
      * The actual code for the SubCommand
